807ca22b6897fe6dad369f199fbf7fad40bf771a
Elias Svensson <c24esn@cs.umu.se>
2025-10-27 20:41:55 +0100

    remove iterations, use date

--- test_mdu.sh
+++ test_mdu.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 
-run_prog() { # [iterations] [threads] [dir]
-  for ((i = 1; i <= $1; i++)); do
-    ./mdu_competition $3 -j $2;
-  done;
-}
-
 if [[ $# -ne 3 ]]; then
-  echo "usage: $0 [ITERATIONS] [MAX THREAD COUNT] [DIR]"
+  echo "usage: $0 [START THREADS][MAX THREAD COUNT] [DIR]"
   exit
 fi
 
 log_file="mdu_test.log"
 test_dir=$3
-iterations=$1
 max_threads=$2
+start_threads=$1
 
 echo "----- New test -----" >> $log_file
-echo "Iterations: $iterations    Threads: $max_threads" >> $log_file
+echo "Threads: $start_threads - $max_threads    Dir: $test_dir" >> $log_file
 
 fastest_time=10000
-for ((i = 1; i <= $max_threads; i++)); do
-  timeout=$( { time run_prog "$iterations" "$i" "$test_dir"; } 2>&1 )
-  fmt=$(echo "$timeout" | grep 'real' | awk '{print $2}' | sed s/s// | sed s/.m//)
+for ((i = $start_threads; i <= $max_threads; i++)); do
+  start=$(date +%s.%N)
+  ./mdu_competition -j "$i" "$test_dir" >/dev/null
+  end=$(date +%s.%N)
+  fmt=$(echo "$end - $start" | bc -l)
+  printf "%d %.6f\n" "$i" "$fmt"
 
   if (( $(echo "$fmt < $fastest_time" | bc -l) )); then
-    fastest_time="$fmt"
-    fastest_threads="$i"
+    fastest_time=$fmt
+    fastest_threads=$i
   fi
-
-  echo "$i $fmt" >> $log_file
 done;
 
 echo "Saved results to $log_file"
